3365599f1cc9e7b2b9fa8267c25a51c3020d4f3d
nbagonoc <nbagonoc@gmail.com>
2017-08-01 13:46:30 +0800

    version 1

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- important heading -->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="favicon.ico">
+  <title>project</title>
+  <meta name="description" content="welcome to my website" />
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/yamm.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+  <link href="css/animate.css" rel="stylesheet">
+  <!-- <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:bold|Roboto" rel="stylesheet">  google font -->
+</head>
+
+<body>
+
+  <!--NAVIGATION-->
+<div class="navbar navbar-default navbar-fixed-top yamm">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" data-toggle="collapse" data-target="#navbar-collapse-grid" class="navbar-toggle">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button><a href="#" class="navbar-brand">LogoHere</a>
+        </div>
+        <div id="navbar-collapse-grid" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="">LINK-A</a></li>
+                <li><a href="">LINK-B</a></li>
+                <li><a href="">LINK-C</a></li>
+                <li><a href="">LINK-D</a></li>
+            </ul>
+            <div class="mainMenu">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="">ABOUT</a></li>
+                    <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle">PRODUCTS<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li class="grid">
+                            <div class="row">
+                                <div class="col-sm-4"><br>
+                                    <h2 class="text-uppercase">megamenu</h2>
+                                    <ul class="list-unstyled">
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-4"><br>
+                                    <h2 class="text-uppercase">megamenu</h2>
+                                    <ul class="list-unstyled">
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-4"><br>
+                                    <h2 class="text-uppercase">megamenu</h2>
+                                    <ul class="list-unstyled">
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                        <li><a href="">MEGAMENU SAMPLE LINK</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="">PRODUCTS</a></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+  
+  <div class="footer">
+  </div>
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/viewportchecker.js"></script><!-- viewport checker for triggerAnimation.js -->
+  <script src="js/animationTrigger.js"></script><!-- controls/triggers the animation.css on scroll -->
+  <script src="js/respond.min.js"></script><!-- fix ie 6, 7 and 8 -->
+</body>
+
+</html>
